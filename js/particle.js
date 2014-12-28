f76cfbf7f0b210c9b2e768123aa47ba4b2ecfdcf
Nick Levantis <niklevantis@gmail.com>
2014-12-28 11:32:03 +0200

    tr

--- js/particle.js
+++ js/particle.js
@@ -6,7 +6,7 @@
 $(document).ready(function() {
   $('.side-left').particleground({
     dotColor: '#000000',
-    lineColor: '#111111',
+    lineColor: '#151515',
     lineWidth: '8.2111',
     particleRadius: '4.1',
     parallaxMultiplier: '5'
