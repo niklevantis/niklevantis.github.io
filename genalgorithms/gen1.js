222ed1ed739adb2d984cd8a944fcd5b012ccc8e6
niklevantis <niklevantis@gmail.com>
2023-02-22 19:45:16 +0200

    change6

--- genalgorithms/gen1.js
+++ genalgorithms/gen1.js
@@ -1,23 +1,23 @@
-let mystic = {c:2, ds:6, f:5, fs:3, g:10, as:7};
-let beat = [8,8,8,8,16,16,16,16,8,8,8];
+let mystic = {c:2, ds:6, f:5, fs:2, g:9, as:7};
+let beat = [8];
 let scaleLength = Object.values(mystic).length;
 
 var ascending = true;
 
 
 let f = _ => {
-  if (round(rand(0, countTo(scaleLength-1))) == 0) {
+  if (round(rand(0, countTo(scaleLength-3))) == 0) {
     ascending = !ascending;
   }
 
   if (ascending) {
-    return {note:countTo(scaleLength-1), vel:countTo(scaleLength-1)>=2 && countTo(scaleLength-1)<=4? 60: 59, oct:3};
+    return {note:countTo(scaleLength-1), vel:countTo(scaleLength-1)>=3 && countTo(scaleLength-1)<=1? 60: 59, oct:5.01};
   } else {
-    return {note:countFrom(scaleLength-1), vel:countFrom(scaleLength-1)>=1 && countFrom(scaleLength-1)<=4? 40: 1, oct:(rand(4,4.01))};
+    return {note:countFrom(scaleLength-1), vel:countFrom(scaleLength-1)>=1 && countFrom(scaleLength-1)<=3? 40: 39, oct:5};
   }
 }
 
 
-Sequencer.generator(mystic, f).synth('Synth',{a:0,d:0,r:0.7}).start(ac,round(rand(60,60)), beat);
+Sequencer.generator(mystic, f).synth('Synth',{a:0,d:0,r:0.5}).start(ac,round(rand(160,160)), beat);
 
 
